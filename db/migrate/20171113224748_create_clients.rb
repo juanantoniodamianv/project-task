5e23268adc117a5b381b1426a4b4eef2310f5e50
Juan Antonio Damian Vargas <juanantoniodamianv@gmail.com>
2017-11-13 19:48:34 -0300

    Create scaffold for Clients.

---
+++ db/migrate/20171113224748_create_clients.rb
@@ -0,0 +1,13 @@
+class CreateClients < ActiveRecord::Migration[5.0]
+  def change
+    create_table :clients do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :address
+      t.string :telephone
+      t.string :document_number
+
+      t.timestamps
+    end
+  end
+end
